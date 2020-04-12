f3bebe4cb3e220f30b2a5bc399aaf52414415e9b
VictoriaSong98 <Vicria0410>
2020-04-12 19:32:04 -0400

    Phase one

---
+++ models/club.js
@@ -0,0 +1,18 @@
+// Model for the club collection.
+const mongoose = require('mongoose');
+
+// Define the schema
+const Club = new mongoose.Schema({
+  _id: String,
+  category: String,
+  introduction: String,
+  meetingTime:String,
+  meetingLocation: String,
+  comingEvent: String,
+  officerList: [String],
+  memberList: [String],
+  passEvent: String
+});
+
+// Export the model
+module.exports = mongoose.model('Club', Club);
