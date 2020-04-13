2a8aa68a997c5d17e1938578a76d36da27e508b9
VictoriaSong98 <Vicria0410>
2020-04-13 00:22:37 -0400

    Set up the web server

---
+++ controllers/clubs.js
@@ -0,0 +1,25 @@
+// Controller for the club collection.
+const Course = require('../models/club');
+
+// GET /clubs
+module.exports.index = function(request, response, next) {
+  Course.distinct('_id')
+    .then(clubIDs => response.redirect(`/clubs/${clubIDs[0]}`))
+    .catch(error => next(error));
+};
+
+// GET /clubs/:id
+module.exports.retrieve = function(request, response, next) {
+  const queries = [
+    Course.findById(request.params.id),
+    Course.distinct('_id')
+  ];
+
+  Promise.all(queries).then(function([club, clubIDs]) {
+    if (club) {
+      response.render('clubs/index', {club: club, clubIDs: clubIDs});
+    } else {
+      next(); // No such club
+    }
+  }).catch(error => next(error));
+};
