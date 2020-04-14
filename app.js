132ede41c7606e1d3262c5bb40346a3a033f7aa2
VictoriaSong98 <Vicria0410>
2020-04-13 23:01:59 -0400

    Phase one

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ app.use(function(request, response, next) {
 
 // Redirect from the home page
 app.get('/', function(request, response) {
-  response.redirect('/courses');
+  response.redirect('/clubs');
 });
 
 // Route content requests
