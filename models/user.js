0a741d2002aafd6199513fe28a174ddfb8ce7f7c
VictoriaSong98 <Vicria0410>
2020-04-27 19:52:44 -0400

    drop my_club and my_theme_house

--- models/user.js
+++ models/user.js
@@ -4,8 +4,6 @@ const mongoose = require('mongoose');
 // Define the schema
 const User = new mongoose.Schema({
   _id: String,
-  my_club: [String],
-  my_theme_house: String,
   club_theme_house_created_by_me: [String]
 });
 
