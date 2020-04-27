f00f5bc748f8595cd438acb6f0db7a6d5afc2b5a
tpark16 <tpark16@stlawu.edu>
2020-04-27 01:28:42 -0400

    app.js login

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const session = require('express-session');
 const router = require('./router');
 const connect = require('./db');
 
@@ -11,6 +12,17 @@ const app = express();
 // Configure the views
 app.set('view engine', 'ejs');
 app.set('views', './views');
+
+// Parse request bodies like query strings
+app.use(express.urlencoded({extended: false}));
+
+// Generate a session for each client
+app.use(session({
+  name: 'catalog', // Name of client cookies
+  secret: 'temporary', // Password for client cookies
+  resave: false, // Recommended setting
+  saveUninitialized: false // Recommended setting
+}));
 
 // Ignore icon requests
 app.get('/favicon.ico', function(request, response) {
@@ -28,6 +40,24 @@ app.use(function(request, response, next) {
 // Redirect from the home page
 app.get('/', function(request, response) {
   response.redirect('/clubs');
+});
+
+// Enter admin mode and return to the previous page
+app.get('/login', function(request, response) {
+  request.session.admin = true;
+  response.redirect('back');
+});
+
+// Exit admin mode and return to the previous page
+app.get('/logout', function(request, response) {
+  request.session.admin = false;
+  response.redirect('back');
+});
+
+// Make the mode available in all views
+app.use(function(request, response, next) {
+  response.locals.admin = request.session.admin;
+  next();
 });
 
 // Route content requests
