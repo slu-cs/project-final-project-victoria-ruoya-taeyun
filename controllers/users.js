1e9c24a4f7830c12f6da97ac458d9586dd84de13
Acelyn <Acelyn@users.noreply.github.com>
2020-05-06 16:05:13 -0400

    s

--- controllers/users.js
+++ controllers/users.js
@@ -3,7 +3,7 @@ const Users = require('../models/user');
 
 // POST /login (with a user ID in the request body)
 module.exports.login = function(request, response, next) {
-  User.findById(request.body.id)
+  Users.findById(request.body.id)
     .then(function(user) {
       if (user) {
         request.session.user = user;
