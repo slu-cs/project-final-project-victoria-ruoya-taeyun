3db5cf77e60bf59216a9333824e1b7d222f2ae11
Acelyn <Acelyn@users.noreply.github.com>
2020-05-05 23:52:11 -0400

    log in home page

--- router.js
+++ router.js
@@ -6,6 +6,20 @@ const users = require('./controllers/users');
 // Create the router
 const router = express.Router();
 
+// Handle home-page requests
+router.get('/', function(request, response) {
+  response.render('index');
+});
+
+// Handle login requests
+router.post('/login', users.login);
+
+// Handle logout requests
+router.get('/logout', function(request, response) {
+  request.session.user = undefined;
+  response.redirect('/');
+});
+
 // Check for admin status
 const authorize = function(request, response, next) {
   if (request.session.admin) {
