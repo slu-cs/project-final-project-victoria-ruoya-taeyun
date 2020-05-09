3cc8dfba73636f9df26c77c39a3044790d57a2fa
Acelyn <Acelyn@users.noreply.github.com>
2020-05-09 14:21:23 -0400

    .

--- controllers/users.js
+++ controllers/users.js
@@ -15,14 +15,14 @@ module.exports.login = function(request, response, next) {
     }).catch(error => next(error));
 };
 module.exports.signup = function(request, response, next) {
-  const new_id = request.body
+  const new_id = request.body;
   const userList = User.map(user=>user._id);
   if (userList.indexOf(new_id)<0) {
     new User ({_id: request.body.id, club_theme_house_created_by_me: []})
   }
 
     }.catch(error => next(error));
-};
+
 
 
 //module.exports.signup = function(request, response, next) {
