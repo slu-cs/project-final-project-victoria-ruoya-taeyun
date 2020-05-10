0e70fa24131b4234d687ece498070a875494a4ed
Acelyn <Acelyn@users.noreply.github.com>
2020-05-09 22:06:29 -0400

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const session = require('express-session');
 const router = require('./router');
 const connect = require('./db');
-
+app.use(express.static(path.join(__dirname, 'picture')));
 // Connect to the database
 connect();
 
