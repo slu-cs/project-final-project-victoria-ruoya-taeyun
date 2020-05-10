99ab36b252fb8f2c1bc9191130ee7ee7633f4d99
Acelyn <Acelyn@users.noreply.github.com>
2020-05-10 16:06:24 -0400

    w

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -59,7 +59,7 @@ module.exports.newMember = function(request, response, next) {
   //const curClub = lst[-1];
   club = Club.findById(request.body);
 //club = club.replace(/'/g,"");
-  console.log(typeof(club));
+  console.log(typeof(club.id));
 
   Club.findById(request.body).distinct('memberList').
   then(function(memberList){
