2c5436f9c4c9bbf661ba13950ca3bb8dd6bd0c9c
Acelyn <Acelyn@users.noreply.github.com>
2020-05-13 15:30:51 -0400

    _id

--- controllers/users.js
+++ controllers/users.js
@@ -4,10 +4,9 @@ const Club = require('../models/club');
 
 // POST /login (with a user ID in the request body)
 module.exports.login = function(request, response, next) {
-  User.findById(request.body.id)
+  User.findById(request.body._id)
     .then(function(user) {
       if (user) {
-        console.log(request.body.id);
         request.session.user = user;
         response.status(200).end();
       } else {
