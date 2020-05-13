b55e8b655be1c0df6dbbb32a2fbb051a2fe4a01b
Acelyn <Acelyn@users.noreply.github.com>
2020-05-13 16:01:54 -0400

    awsl

--- controllers/clubs.js
+++ controllers/clubs.js
@@ -66,6 +66,7 @@ module.exports.delete = function(request, response, next) {
 
 
 module.exports.join = function(request, response, next) {
+  console.log("awsl");
     Club.findByIdAndUpdate(request.params.id, {$push: {memberList: request.session.user._id}},{runValidators: true})
   .then(function(club){ if(club) {response.status(200).end();} else{next()}})
     .catch(error => next(error));
